3e6c0a697c1cc93639b0501dd05f3a94a73a6882
ali bozna <alibozna@gmail.com>
2021-07-20 23:09:31 +0200

    BALC-10: Migrate MahApps to the project.

--- sources/Bali.Converter.App/MainWindow.xaml.cs
+++ sources/Bali.Converter.App/MainWindow.xaml.cs
@@ -4,18 +4,23 @@
     using System.Windows.Controls;
 
     using Bali.Converter.App.Modules.MediaDownloader.Views;
+    using ControlzEx.Theming;
+    using MahApps.Metro.Controls;
 
     using Prism.Regions;
 
     using Unity;
 
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         private readonly IRegionManager regionManager;
 
         public MainWindow(IRegionManager regionManager, IUnityContainer container)
         {
             this.InitializeComponent();
+
+            ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncAll;
+            ThemeManager.Current.SyncTheme();
 
             this.regionManager = regionManager;
             this.regionManager.RegisterViewWithRegion("ContentRegion", () => container.Resolve<MediaDownloaderView>());
