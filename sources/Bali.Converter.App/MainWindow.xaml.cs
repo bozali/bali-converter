da8d5cddfa9797018794880069f70806439065f9
ali bozna <alibozna@gmail.com>
2021-07-22 02:00:24 +0200

    BALC-2: Added navigation view without icons.

--- sources/Bali.Converter.App/MainWindow.xaml.cs
+++ sources/Bali.Converter.App/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿namespace Bali.Converter.App
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Windows;
     using System.Windows.Controls;
-
+    using System.Windows.Media;
     using Bali.Converter.App.Modules.MediaDownloader.Views;
     using ControlzEx.Theming;
     using MahApps.Metro.Controls;
@@ -23,14 +25,31 @@
             ThemeManager.Current.SyncTheme();
 
             this.regionManager = regionManager;
+
             this.regionManager.RegisterViewWithRegion("ContentRegion", () => container.Resolve<MediaDownloaderView>());
         }
-
-        private void NavigationButtonClicked(object sender, RoutedEventArgs e)
+        
+        private void NavigationItemInvoked(object sender, HamburgerMenuItemInvokedEventArgs args)
         {
-            var item = (Button)sender;
-
+            var item = (HamburgerMenuItem)args.InvokedItem;
             this.regionManager.Regions["ContentRegion"].RequestNavigate(item.Tag.ToString());
+        }
+
+        public IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj != null)
+            {
+                for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+                {
+                    DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+
+                    if (child != null && child is T)
+                        yield return (T)child;
+
+                    foreach (T childOfChild in FindVisualChildren<T>(child))
+                        yield return childOfChild;
+                }
+            }
         }
     }
 }
